f5113882c67944a9fd79f5c94683631b4375f18d
sandra8891 <sandraks8891@gmail.com>
2025-04-02 16:06:09 +0530

    contact msg

---
+++ home.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template,request
+from flask_mail import Mail, Message
+
+app=Flask(__name__)
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'sandraks8891@gmail.com'
+app.config['MAIL_PASSWORD'] = 'bblq jsjt posk gwpl'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
+mail = Mail(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')    
+
+@app.route('/send_mail',methods=['POST'])
+def send_message():
+    name=request.form['name']
+    email=request.form['email']
+    message=request.form['message']
+    msg = Message(subject='Hello', sender=email, recipients=['sandraks8891@gmail.com'])
+    msg.body = f"From: {name}\n {email}\n\nMessage:\n{message}"
+    mail.send(msg)
+    return render_template('index.html', success=True)
+
+
+
+
+app.run()
