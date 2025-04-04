23d024da341a17a1b1c3315c190ca87a095219d7
sandra8891 <sandraks8891@gmail.com>
2025-04-04 10:56:42 +0530

    contact updated

--- home.py
+++ home.py
@@ -4,11 +4,11 @@ from flask_mail import Mail, Message
 app=Flask(__name__)
 
 app.config['MAIL_SERVER']='smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
+app.config['MAIL_PORT'] = 587
 app.config['MAIL_USERNAME'] = 'sandraks8891@gmail.com'
 app.config['MAIL_PASSWORD'] =  'gupz xzhw rzne ywtc'
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
 
 mail = Mail(app)
 
